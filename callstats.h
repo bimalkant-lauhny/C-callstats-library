c5dcd53d5e2b1248b3a4ea37aa61bf5018ede5b1
code_master5 <lauhny.bimalk@gmail.com>
2017-08-02 19:15:00 +0530

    Added Copyright

---
+++ callstats.h
@@ -0,0 +1,6 @@
+/* \file callstats.h
+ * \author Bimalkant Lauhny <lauhny.bimalk@gmail.com>
+ * \copyright
+ * \brief header file defining functions for sending events to callstast.io REST
+ * API.
+ */
