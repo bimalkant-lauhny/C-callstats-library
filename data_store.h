6b11e4e6812ee21f9e16054aeff0f54361705b20
code_master5 <lauhny.bimalk@gmail.com>
2017-08-10 22:45:40 +0530

    Added get_user_infor for info retrieval

--- data_store.h
+++ data_store.h
@@ -36,6 +36,20 @@ void initialize_user_info(user_info *user) {
     user->remote_candidate = NULL;
 }
 
+void free_user_info(user_info *user) {
+    free(user->user_num);
+    free(user->user_id);
+    free(user->conf_num);
+    free(user->conf_id);
+    free(user->device_id);
+    free(user->session_id);
+    free(user->handle_id);
+    free(user->audio_ssrc);
+    free(user->video_ssrc);
+    free(user->local_candidate);
+    free(user->remote_candidate);
+}
+
 size_t initialize_db() {
     if (db != NULL) {
         return 0;
@@ -138,6 +152,87 @@ size_t add_user_num(char *session_id, char *handle_id, char *user_num) {
         return -1;
     } 
     return 1; 
+}
+
+size_t get_user_info(char *session_id, char *handle_id, user_info *user) {
+    char *sql = "SELECT * FROM Stats_Info WHERE session_id='%s' AND handle_id='%s';";
+    char sql_buffer[BUFFER_SIZE_SQLITE];
+    snprintf(sql_buffer, sizeof(sql_buffer), sql, 
+            session_id, handle_id);
+    printf("Buffer: %s\n", sql_buffer);
+    sqlite3_stmt *stmt;
+    size_t rc = sqlite3_prepare_v2(db, sql_buffer, -1, &stmt, NULL);
+    if (rc != SQLITE_OK ) {
+        
+        fprintf(stderr, "Failed to prepare SQL statement.\n");
+        fprintf(stderr, "SQL error: %s\n", err_msg);
+        
+        sqlite3_free(err_msg);
+        sqlite3_close(db);
+        
+        return -1;
+    }  
+    
+    while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
+        const char *res = sqlite3_column_text(stmt, 0);
+        user->user_num = malloc(strlen(res) + 1);
+        strcpy(user->user_num, res);
+        
+        res = sqlite3_column_text(stmt, 1);
+        user->user_id = malloc(strlen(res) + 1);
+        strcpy(user->user_id, res);
+        
+        res = sqlite3_column_text(stmt, 2);
+        user->conf_num= malloc(strlen(res) + 1);
+        strcpy(user->conf_num, res);
+        
+        res = sqlite3_column_text(stmt, 3);
+        user->conf_id = malloc(strlen(res) + 1);
+        strcpy(user->conf_id, res);
+        
+        res = sqlite3_column_text(stmt, 4);
+        user->device_id = malloc(strlen(res) + 1);
+        strcpy(user->device_id, res);
+        
+        res = sqlite3_column_text(stmt, 5);
+        user->session_id = malloc(strlen(res) + 1);
+        strcpy(user->session_id, res);
+            
+        
+        res = sqlite3_column_text(stmt, 6);
+        user->handle_id = malloc(strlen(res) + 1);
+        strcpy(user->handle_id, res);
+        
+        res = sqlite3_column_text(stmt, 7);
+        user->audio_ssrc = malloc(strlen(res) + 1);
+        strcpy(user->audio_ssrc, res);
+        
+        res = sqlite3_column_text(stmt, 8);
+        user->video_ssrc = malloc(strlen(res) + 1);
+        strcpy(user->video_ssrc, res);
+        
+        res = sqlite3_column_text(stmt, 9);
+        user->local_candidate = malloc(strlen(res) + 1);
+        strcpy(user->local_candidate, res);
+        
+        res = sqlite3_column_text(stmt, 10);
+        user->remote_candidate = malloc(strlen(res) + 1);
+        strcpy(user->remote_candidate, res);
+    }
+    
+    if (rc != SQLITE_DONE) {
+        fprintf(stderr, "Failed to prepare SQL statement.\n");
+        fprintf(stderr, "SQL error: %s\n", err_msg);
+        
+        sqlite3_free(err_msg);
+        sqlite3_close(db);
+        
+        return -1;
+    }
+    
+    sqlite3_finalize(stmt);
+    
+    return 0;
 }
 
 size_t close_db() {
